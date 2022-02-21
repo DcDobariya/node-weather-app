6ececb0beea570c56b7ecc8fd8053ea90ccaa0e4
Harsh Dobariya <hdobariya229@gmail.com>
2022-02-21 11:24:30 +0530

    Add new features in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,8 +8,16 @@ module.exports = (latitude, longitude, callback) => {
         .then(({ data }) => {
             if (data.error) return callback("Unable to find location. Try another search.", undefined);
 
-            const { temperature, precip } = data.current || {};
-            callback(undefined, `It is currently ${temperature} degrees out. There is a ${precip}% chance of rain.`);
+            const date = new Date(Date.now() - 86400000).toISOString().slice(0, 10);
+
+            const {
+                current: { temperature, precip },
+                forecast: {
+                    [date]: { maxtemp, mintemp }
+                }
+            } = data || {};
+
+            callback(undefined, `It is currently ${temperature} degrees out. The high temp. of today is ${maxtemp} and low temp. of today ${mintemp} There is a ${precip}% chance of rain.`);
         })
         .catch((err) => callback(err.message, undefined));
 };
